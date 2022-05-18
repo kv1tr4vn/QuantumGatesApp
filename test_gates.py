b8d3a66ed4b03c96321e50de210ef50520d4c0e2
kv1tr4vn <kv1tr4vn@gmail.com>
2022-05-18 18:36:10 +0300

    Add one more test for phase

--- test_gates.py
+++ test_gates.py
@@ -14,12 +14,34 @@ class TestGates(unittest.TestCase):
         control_qubit_ind = 0
         target_qubit_ind = 1
 
-        result_array = apply_phase(input_array, phase, control_qubit_ind, target_qubit_ind)
+        result_array = apply_phase(input_array, control_qubit_ind, target_qubit_ind, phase)
         expected_array = np.array([
             'c1 + c1 * I',
             'c2 + c2 * I',
             'c3 + c3 * I',
             'c4 + c4 * I'], dtype='U1000')
+
+        self.assertTrue(result_array.size == expected_array.size)
+        for array_ind in range(expected_array.size):
+            self.assertTrue((sp.simplify(result_array[array_ind] + ' - (' + expected_array[array_ind] + ')') == 0))
+
+    def test_apply_phase_2(self):
+        input_array = np.array([
+            'c1 + c1 * I',
+            'c2 + c2 * I',
+            'c3 + c3 * I',
+            'c4 + c4 * I'], dtype='U1000')
+
+        phase = 10.0
+        control_qubit_ind = 0
+        target_qubit_ind = 1
+
+        result_array = apply_phase(input_array, control_qubit_ind, target_qubit_ind, phase)
+        expected_array = np.array([
+            'c1 + c1 * I',
+            'c2 + c2 * I',
+            'c3 + c3 * I',
+            '(1 - 1.2246467991473533e-15*I)*(c4 + I*c4)'], dtype='U1000')
 
         self.assertTrue(result_array.size == expected_array.size)
         for array_ind in range(expected_array.size):
