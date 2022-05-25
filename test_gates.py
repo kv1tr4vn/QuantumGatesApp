d03b5e1e08132a6f31c52bb288d0a10d22fc9833
Egor Kiligin <44437639+Egoridze@users.noreply.github.com>
2022-05-26 00:26:11 +0300

    Tested sigma_z 3 qubits on first and second qubit

--- test_gates.py
+++ test_gates.py
@@ -208,8 +208,8 @@ class TestGates(unittest.TestCase):
             'I*(c4 + c4 * j)',
             '-I*(c1 + c1 * j)',
             '-I*(c2 + c2 * j)',
-            '+I*(c7 + c7 * j)',
-            '+I*(c8 + c8 * j)',
+            'I*(c7 + c7 * j)',
+            'I*(c8 + c8 * j)',
             '-I*(c5 + c5 * j)',
             '-I*(c6 + c6 * j)'], dtype='U1000')
 
@@ -238,6 +238,34 @@ class TestGates(unittest.TestCase):
         for array_ind in range(expected_array.size):
             self.assertTrue((sp.simplify(result_array[array_ind] + ' - (' + expected_array[array_ind] + ')') == 0))
 
+    def test_apply_sigma_z_3_1(self):
+        input_array = np.array([
+            'c1 + c1 * I',
+            'c2 + c2 * I',
+            'c3 + c3 * I',
+            'c4 + c4 * I',
+            'c5 + c5 * I',
+            'c6 + c6 * I',
+            'c7 + c7 * I',
+            'c8 + c8 * I'], dtype='U1000')
+
+        target_qubit_ind = 0
+        result_array = apply_sigma_z(input_array, target_qubit_ind)
+
+        expected_array = np.array([
+            'c1 + c1 * j',
+            'c2 + c2 * j',
+            'c3 + c3 * j',
+            'c4 + c4 * j',
+            '-1*(c5 + c5 * j)',
+            '-1*(c6 + c6 * j)',
+            '-1*(c7 + c7 * j)',
+            '-1*(c8 + c8 * j)'], dtype='U1000')
+
+        self.assertTrue(result_array.size == expected_array.size)
+        for array_ind in range(expected_array.size):
+            self.assertTrue((sp.simplify(result_array[array_ind] + ' - (' + expected_array[array_ind] + ')') == 0))
+
     def test_apply_sigma_z_2(self):
         input_array = np.array([
             'c1 + c1 * I',
@@ -254,6 +282,34 @@ class TestGates(unittest.TestCase):
             '-1*(c2 + c2 * j)',
             'c3 + c3 * j',
             '-1*(c4 + c4 * j)'], dtype='U1000')
+
+        self.assertTrue(result_array.size == expected_array.size)
+        for array_ind in range(expected_array.size):
+            self.assertTrue((sp.simplify(result_array[array_ind] + ' - (' + expected_array[array_ind] + ')') == 0))
+
+    def test_apply_sigma_z_3_2(self):
+        input_array = np.array([
+            'c1 + c1 * I',
+            'c2 + c2 * I',
+            'c3 + c3 * I',
+            'c4 + c4 * I',
+            'c5 + c5 * I',
+            'c6 + c6 * I',
+            'c7 + c7 * I',
+            'c8 + c8 * I'], dtype='U1000')
+
+        target_qubit_ind = 1
+
+        result_array = apply_sigma_z(input_array, target_qubit_ind)
+        expected_array = np.array([
+            'c1 + c1 * j',
+            'c2 + c2 * j',
+            '-1*(c3 + c3 * j)',
+            '-1*(c4 + c4 * j)',
+            'c5 + c5 * j',
+            'c6 + c6 * j',
+            '-1*(c7 + c7 * j)',
+            '-1*(c8 + c8 * j)'], dtype='U1000')
 
         self.assertTrue(result_array.size == expected_array.size)
         for array_ind in range(expected_array.size):
