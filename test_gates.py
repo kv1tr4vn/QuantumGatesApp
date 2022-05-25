b94fdd83ab97711d83c22837f508bb5f9c19641c
Egor Kiligin <44437639+Egoridze@users.noreply.github.com>
2022-05-25 23:05:26 +0300

    Tested cnot, ccnot, sigma_X

--- test_gates.py
+++ test_gates.py
@@ -89,6 +89,35 @@ class TestGates(unittest.TestCase):
         for array_ind in range(expected_array.size):
             self.assertTrue((sp.simplify(result_array[array_ind] + ' - (' + expected_array[array_ind] + ')') == 0))
 
+    def test_apply_sigma_x_3_2(self):
+        input_array = np.array([
+            'c1 + c1 * I',
+            'c2 + c2 * I',
+            'c3 + c3 * I',
+            'c4 + c4 * I',
+            'c5 + c5 * I',
+            'c6 + c6 * I',
+            'c7 + c7 * I',
+            'c8 + c8 * I'], dtype='U1000')
+
+        target_qubit_ind = 1
+
+        result_array = apply_sigma_x(input_array, target_qubit_ind)
+
+        expected_array = np.array([
+            'c3 + c3 * j',
+            'c4 + c4 * j',
+            'c1 + c1 * j',
+            'c2 + c2 * j',
+            'c7 + c7 * j',
+            'c8 + c8 * j',
+            'c5 + c5 * j',
+            'c6 + c6 * j'], dtype='U1000')
+
+        self.assertTrue(result_array.size == expected_array.size)
+        for array_ind in range(expected_array.size):
+            self.assertTrue((sp.simplify(result_array[array_ind] + ' - (' + expected_array[array_ind] + ')') == 0))
+
     def test_apply_sigma_y_1(self):
         input_array = np.array([
             'c1 + c1 * I',
@@ -261,6 +290,138 @@ class TestGates(unittest.TestCase):
             '(c1 + c1*j - c2 - c2*j + c3 + c3*j - c4 - c4*j) / sqrt(4)',
             '(c1 + c1*j + c2 + c2*j - c3 - c3*j - c4 - c4*j) / sqrt(4)',
             '(c1 + c1*j - c2 - c2*j - c3 - c3*j + c4 + c4*j) / sqrt(4)'], dtype='U1000')
+
+        self.assertTrue(result_array.size == expected_array.size)
+        for array_ind in range(expected_array.size):
+            self.assertTrue((sp.simplify(result_array[array_ind] + ' - (' + expected_array[array_ind] + ')') == 0))
+
+    def test_apply_cnot_2_1_2(self):
+        input_array = np.array([
+            'c1 + c1 * I',
+            'c2 + c2 * I',
+            'c3 + c3 * I',
+            'c4 + c4 * I'], dtype='U1000')
+
+        control_qubit_ind = 0
+        target_qubit_ind = 1
+
+        result_array = apply_cnot(input_array, control_qubit_ind, target_qubit_ind)
+        #print(result_array)
+        expected_array = np.array([
+            'c1 + c1 * I',
+            'c2 + c2 * I',
+            'c4 + c4 * I',
+            'c3 + c3 * I'], dtype='U1000')
+
+        self.assertTrue(result_array.size == expected_array.size)
+        for array_ind in range(expected_array.size):
+            self.assertTrue((sp.simplify(result_array[array_ind] + ' - (' + expected_array[array_ind] + ')') == 0))
+
+    def test_apply_cnot_2_2_1(self):
+        input_array = np.array([
+            'c1 + c1 * I',
+            'c2 + c2 * I',
+            'c3 + c3 * I',
+            'c4 + c4 * I'], dtype='U1000')
+
+        control_qubit_ind = 1
+        target_qubit_ind = 0
+
+        result_array = apply_cnot(input_array, control_qubit_ind, target_qubit_ind)
+        expected_array = np.array([
+            'c1 + c1 * I',
+            'c4 + c4 * I',
+            'c3 + c3 * I',
+            'c2 + c2 * I'], dtype='U1000')
+
+        self.assertTrue(result_array.size == expected_array.size)
+        for array_ind in range(expected_array.size):
+            self.assertTrue((sp.simplify(result_array[array_ind] + ' - (' + expected_array[array_ind] + ')') == 0))
+
+    def test_apply_cnot_3_1_2(self):
+        input_array = np.array([
+            'c1 + c1 * I',
+            'c2 + c2 * I',
+            'c3 + c3 * I',
+            'c4 + c4 * I',
+            'c5 + c5 * I',
+            'c6 + c6 * I',
+            'c7 + c7 * I',
+            'c8 + c8 * I'], dtype='U1000')
+
+        control_qubit_ind = 0
+        target_qubit_ind = 1
+
+        result_array = apply_cnot(input_array, control_qubit_ind, target_qubit_ind)
+        expected_array = np.array([
+            'c1 + c1 * I',
+            'c2 + c2 * I',
+            'c3 + c3 * I',
+            'c4 + c4 * I',
+            'c7 + c7 * I',
+            'c8 + c8 * I',
+            'c5 + c5 * I',
+            'c6 + c6 * I'], dtype='U1000')
+
+        self.assertTrue(result_array.size == expected_array.size)
+        for array_ind in range(expected_array.size):
+            self.assertTrue((sp.simplify(result_array[array_ind] + ' - (' + expected_array[array_ind] + ')') == 0))
+
+    def test_apply_ccnot_1_2_3(self):
+        input_array = np.array([
+            'c1 + c1 * I',
+            'c2 + c2 * I',
+            'c3 + c3 * I',
+            'c4 + c4 * I',
+            'c5 + c5 * I',
+            'c6 + c6 * I',
+            'c7 + c7 * I',
+            'c8 + c8 * I'], dtype='U1000')
+
+        first_control_qubit_ind = 0
+        second_control_qubit_ind = 1
+        target_qubit_ind = 2
+
+        result_array = apply_ccnot(input_array, first_control_qubit_ind, second_control_qubit_ind, target_qubit_ind)
+        expected_array = np.array([
+            'c1 + c1 * I',
+            'c2 + c2 * I',
+            'c3 + c3 * I',
+            'c4 + c4 * I',
+            'c5 + c5 * I',
+            'c6 + c6 * I',
+            'c8 + c8 * I',
+            'c7 + c7 * I'], dtype='U1000')
+
+        self.assertTrue(result_array.size == expected_array.size)
+        for array_ind in range(expected_array.size):
+            self.assertTrue((sp.simplify(result_array[array_ind] + ' - (' + expected_array[array_ind] + ')') == 0))
+
+    def test_apply_ccnot_1_3_2(self):
+        input_array = np.array([
+            'c1 + c1 * I',
+            'c2 + c2 * I',
+            'c3 + c3 * I',
+            'c4 + c4 * I',
+            'c5 + c5 * I',
+            'c6 + c6 * I',
+            'c7 + c7 * I',
+            'c8 + c8 * I'], dtype='U1000')
+
+        first_control_qubit_ind = 0
+        second_control_qubit_ind = 2
+        target_qubit_ind = 1
+
+        result_array = apply_ccnot(input_array, first_control_qubit_ind, second_control_qubit_ind, target_qubit_ind)
+        expected_array = np.array([
+            'c1 + c1 * I',
+            'c2 + c2 * I',
+            'c3 + c3 * I',
+            'c4 + c4 * I',
+            'c5 + c5 * I',
+            'c8 + c8 * I',
+            'c7 + c7 * I',
+            'c6 + c6 * I'], dtype='U1000')
 
         self.assertTrue(result_array.size == expected_array.size)
         for array_ind in range(expected_array.size):
